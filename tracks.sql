7e71daed6b18d1f675d94b39a26d88a2ba72aca1
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-11-02 19:07:36 -0800

    Add files via upload

---
+++ tracks.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeeTracker;
+CREATE DATABASE employeeTracker;
+USE employeeTracker;
+CREATE TABLE department(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+) ;
+CREATE TABLE role(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employee(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO department (name) values ('Human');
+INSERT INTO department (name) values ('Brainstorming');
+INSERT INTO role (title, salary) values ('Thinker', 5 );  
+INSERT INTO role (title, salary) values ('brain', 7 );  
